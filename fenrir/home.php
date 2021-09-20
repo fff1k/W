a3134516a9aad56aeb0c99097ebdbdb253da9e7c
luckypancher <90251215+fff1k@users.noreply.github.com>
2021-09-20 19:08:25 +0300

    Add files via upload

---
+++ fenrir/home.php
@@ -0,0 +1,357 @@
+
+
+
+<head>
+
+<title>OSINT</title>
+
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@400;500;700&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+<div class="navbar">
+        
+        <div class="logo">
+            <img src="logo.png">
+        </div>
+
+        <ul class="desc_menu"> 
+            <li><a href="#top">OSINT</a></li>
+            <li><a href="#contacts">Контакты</a></li>
+        </ul>
+
+          
+          
+
+    </div>
+
+<div class="container-fluid">
+    <a name="top"></a>
+    <div class="about">
+        
+        <div class="about_logo">
+            <img src="Logo_div.png"> 
+        </div>
+
+        <div class="about_text">
+            
+            <h1>
+                OSINT
+            </h1>
+
+            <p>
+                OSINT - конкурентная (деловая) разведка, метод получения информации из открытых источников. 
+                OSINT расследования включают в себя сбор и анализ информации о человеке либо компании с последующим составлением отчета и предоставления его заказчику. 
+                Ключевой особенностью данной дисциплины является легальный способ получения информации. Мы действуем в рамках закона. 
+                Обращение в агентство конкурентной разведки гарантирует вам абсолютную анонимность - объект расследования даже не заподозрит, что на него собирается досье. Тем более, кем.
+            </p>
+            
+        </div>
+       
+    </div>
+    
+</div>
+
+<div class="wrapper">
+
+<div class="head_block">
+    Сферы проведения экспертиз
+</div>
+
+
+<div class="field_of_activity">
+
+    <div class="pict_foa">
+        <img src="UR1.png">
+    </div>
+
+    <div class="text_foa">
+        <p><b>Юридические лица</b></p>
+
+        <p>Воспользоваться услугами кибер-разведки могут как государственные, так и частные учреждения. Мы предлагаем исследования для IT, финансового, промышленного и фармакологического сектора. 
+            Методы получения информации выбираются индивидуально в зависимости от потребностей заказчика. 
+            Мы обладаем доступам ко множеству баз данных, а также ведём собственные разработки в области программного обеспечения для проведения OSINT расследований.
+
+        </p>
+    </div>
+
+</div>
+
+<div class="field_of_activity">
+
+    <div class="pict_foa">
+        <img src="FIZ1.png">
+    </div>
+
+    <div class="text_foa">
+        <p><b>Физические лица</b></p>
+        <p>OSINT расследования относительно физических лиц могут помочь HR-специалистам принять верное решение относительно рассматриваемого кандидата. 
+            Помимо этого мы можем составить оценку деловой репутации, компетенций и благонадёжности, а также составить психологический портрет. 
+            Выявим сомнительное прошлое и связи объекта. Предоставим контакные данные.
+
+        </p>
+    </div>
+
+</div>
+
+
+</div>
+
+
+<div class="container-fluid">
+
+<div class="services">
+
+    <div class="services_block" style="background-color: #333335">
+
+        <p>
+           <b> Анализ личности </b>
+        </p>
+
+        <p>
+            Систематизация информации о физическом лице: потенциальном сотруднике, партнере или клиенте.
+        </p>
+
+        
+    </div>
+
+    <div class="services_block"  style="background-color: #29282d">
+
+        <p>
+            <b>Исследования рынка</b>
+        </p>
+
+        <p>
+            Изучение конкурентов, анализ ниши. Рекомендации по позиционированию на рынке. 
+        </p>
+
+        
+    </div>
+
+    <div class="services_block" style="background-color: #333335">
+
+        <p>
+            <b>Аналитика компании</b>
+        </p>
+
+        <p>
+            Проверка информации о компании, анализ благонадёжности контрагента, выявление фактов мошенничества. OSINT консалтинг.
+        </p>
+
+        
+    </div>
+   
+</div>
+
+</div>
+
+<div class="wrapper">
+
+<div class="head_block">
+    Для кого актуальны наши услуги
+</div>
+
+
+<div class="field_of_activity">
+
+    
+
+    <div class="text_foa">
+        
+        <p>Технологии OSINT будут полезны как крупным компаниям, так и частным лицам. </p> 
+        <p>Данная техника разведки помогает:</p> 
+        <ul>
+            <li>Найти компрометирующую информацию о конкурентах</li>
+            <li>Всесторонне оценить соискателя на должность</li>
+            <li>Понять, можно ли доверять вашему бизнес-партнеру</li>
+            <li>Узнать что угодно. О ком угодно</li>
+        </ul>
+
+    </div>
+
+    <div class="pict_foa">
+        <img src="5u.png">
+    </div>
+
+</div>
+
+
+</div>
+
+<div>
+
+</div>
+
+<div class="how_a_work_cont">
+
+<div class="wrapper">
+
+<div class="head_block" style="color:#ebaf11">
+    Как мы работаем?
+</div>
+
+<div class="How_a_work">
+
+    <div class="block_how_a_work">
+        
+        <span>
+            1
+        </span>
+
+        <p> 
+            <b>Принимаем запрос</b>
+
+            <br>
+            <br>
+
+           Изучаем потребности заказчика, выявляем оптимальные методы для проведения исследования. 
+
+        </p>
+
+    </div>
+
+    <div class="block_how_a_work">
+        
+        <span>
+            2
+        </span>
+
+        <p> 
+            <b>Составляем договор</b>
+
+            <br>
+            <br>
+
+            С каждым заказчиком мы заключаем договор о предоставлении услуг, опираясь на индивидуальные потребности каждого клиента. 
+
+        </p>
+
+    </div>
+
+    <div class="block_how_a_work">
+        
+        <span>
+            3
+        </span>
+
+        <p> 
+            <b>Проводим расследование</b>
+
+            <br>
+            <br>
+
+            Команда опытных аналитиков задействует ручные и программные методы исследований, проводит скурпулезный анализ полученных данных и проверяет их подлинность.
+
+        </p>
+
+    </div>
+
+    <div class="block_how_a_work">
+        
+        <span>
+            4
+        </span>
+
+        <p> 
+            <b>Составляем
+                <br> отчёт</b>
+
+            <br>
+            <br>
+
+            Предоставляем заказчику развёрнутый отчёт об объекте. 
+
+        </p>
+
+    </div>
+
+ 
+    
+
+</div>
+</div>
+
+</div>
+
+
+<div class="container-fluid">
+
+<div class="footer">
+
+   <div class="list">
+
+    <span>Контакты</span>
+
+        <ul>
+            <li>Threema</li>
+
+            <li>Telegram</li>
+
+            <li>Email</li>
+
+        </ul>
+        
+   </div>
+
+   <div class="form">
+     
+    <form method="POST" name="form" >
+        
+       <div class="form_block">
+
+        <div class="input_form" name="email">
+            E-mail
+            <input type="text" name="first_name">
+            Имя
+            <input type="text">
+            Объект исследования
+            <select>
+                <option>Юридическая сфера</option>
+                <option>Физические лица</option>
+            </select>
+
+            
+
+            <div class="checkbox_form">
+
+               <span> Какая услуга вас интересует?</span>
+
+            <div class="check_form"> <input type="checkbox"> Ревью человека </div>
+             
+            <div class="check_form"> <input type="checkbox"> Исследование рынка </div>
+
+            <div class="check_form"> <input type="checkbox"> Ревью компании </div>
+
+            </div>
+            Комментарий
+
+            <input type="text">
+
+            <div class="check_form"> <input type="checkbox"> Я согласен с <a href="politics.html">политикой конфедициальности</a></div>
+
+            <button type="submit" >Проконсультироваться</button>
+
+
+        </div>
+
+
+        
+    </form>
+
+   </div>
+
+</div>
+
+</div>
+
+
+
+</body>
+
+<script src="closeNav.js"></script>
